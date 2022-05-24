29050282ac1143e4649ac93f904272153e215ff2
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-24 14:20:02 -0300

    imports CRUDs in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { PalmalModule } from './palmal/palmal.module';
+import { ReboleiraModule } from './reboleira/reboleira.module';
+import { EstacaoModule } from './estacao/estacao.module';
+import { RaqueteModule } from './raquete/raquete.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PalmalModule, ReboleiraModule, EstacaoModule, RaqueteModule],
   controllers: [AppController],
   providers: [AppService],
 })
