45cf995b0983ff50cbb5e0c51b6659222af21db0
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-05-25 00:59:20 -0300

    raquete implements Prisma Entity

--- src/raquete/entities/raquete.entity.ts
+++ src/raquete/entities/raquete.entity.ts
@@ -1,1 +1,15 @@
-export class Raquete {}
+import { Prisma } from '@prisma/client';
+export class Raquete implements Prisma.RaqueteUncheckedCreateInput {
+  id?: number;
+  posicao: string;
+  dataDacastro: string;
+  aparencia: string;
+  agenteControleBiologico: boolean;
+  agenteControleQuimico: boolean;
+  avaliado: boolean;
+  corte?: string;
+  dataObservacao?: string;
+  exported: boolean;
+  nextVersion: number | bigint;
+  estacaoId?: number;
+}
