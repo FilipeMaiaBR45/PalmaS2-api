a449391f56a00e667af370f5c0bd9e73bb5b1bd3
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-05-25 00:59:38 -0300

    reboleira implements Prisma Entity

--- src/reboleira/entities/reboleira.entity.ts
+++ src/reboleira/entities/reboleira.entity.ts
@@ -1,1 +1,12 @@
-export class Reboleira {}
+import { Prisma } from '@prisma/client';
+export class Reboleira implements Prisma.ReboleiraUncheckedCreateInput {
+  id?: number;
+  iteracao: string;
+  dataCadastro: string | Date;
+  proximaAvaliacao?: string;
+  deleted?: string;
+  avaliado: boolean;
+  lastUpdate: string;
+  estacaoes?: Prisma.EstacaoUncheckedCreateNestedManyWithoutReboleiraInput;
+  palmalId?: number;
+}
