c44d2356ec4c97a370d3ad98462c0f9a6d389a58
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-05-25 00:58:54 -0300

    estacao implements Prisma Entity

--- src/estacao/entities/estacao.entity.ts
+++ src/estacao/entities/estacao.entity.ts
@@ -1,1 +1,12 @@
-export class Estacao {}
+import { Prisma } from '@prisma/client';
+export class Estacao implements Prisma.EstacaoUncheckedCreateInput {
+  id?: number;
+  status: string;
+  dataCadastro: string;
+  proximaAvaliacao: string;
+  deleted?: string;
+  isObservationOpen: boolean;
+  lastUpdate: string;
+  reboleiraId?: number;
+  Raquetes?: Prisma.RaqueteUncheckedCreateNestedManyWithoutEstacaoInput;
+}
