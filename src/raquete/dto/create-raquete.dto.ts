c480c9ddc05e4badea6a52b581add4184f4e9303
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-25 17:03:50 -0300

    create raquete DTO

--- src/raquete/dto/create-raquete.dto.ts
+++ src/raquete/dto/create-raquete.dto.ts
@@ -1,1 +1,28 @@
-export class CreateRaqueteDto {}
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { Raquete } from '../entities/raquete.entity';
+
+export class CreateRaqueteDto implements Raquete {
+  id?: number;
+  @IsString()
+  posicao: string;
+  @IsString()
+  dataDacastro: string;
+  @IsString()
+  aparencia: string;
+  @IsBoolean()
+  agenteControleBiologico: boolean;
+  @IsBoolean()
+  agenteControleQuimico: boolean;
+  @IsBoolean()
+  avaliado: boolean;
+  @IsString()
+  corte?: string;
+  @IsString()
+  dataObservacao?: string;
+  @IsBoolean()
+  exported: boolean;
+  @IsNumber()
+  nextVersion: number;
+  @IsNumber()
+  estacaoId?: number;
+}
