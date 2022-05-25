442c1bb815eff544041e3a149ad60c4d491a0b1d
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-05-25 00:58:39 -0300

    palmal implements Prisma Entity

--- src/palmal/entities/palmal.entity.ts
+++ src/palmal/entities/palmal.entity.ts
@@ -1,1 +1,11 @@
-export class Palmal {}
+import { Prisma } from '@prisma/client';
+export class Palmal implements Prisma.PalmalUncheckedCreateInput {
+  id?: number;
+  variedade: string;
+  area: number;
+  tradicional: boolean;
+  dataCadastro: string;
+  lastUpdate: string;
+  reboleiras?: Prisma.ReboleiraUncheckedCreateNestedManyWithoutPalmalInput;
+  userId?: number;
+}
