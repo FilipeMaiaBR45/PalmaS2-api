06a3731dd7cb5fb633b9044030fb6236c53597cc
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-25 17:44:14 -0300

    add PrismaService providers

--- src/palmal/palmal.module.ts
+++ src/palmal/palmal.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PalmalService } from './palmal.service';
 import { PalmalController } from './palmal.controller';
-
+import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   controllers: [PalmalController],
-  providers: [PalmalService]
+  providers: [PalmalService, PrismaService],
+  exports: [PalmalService],
 })
 export class PalmalModule {}
