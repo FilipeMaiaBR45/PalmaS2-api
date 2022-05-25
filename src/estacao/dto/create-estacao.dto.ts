95e6466e49bdab6cd9b7f4d9fde081eee65c9187
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-25 17:03:25 -0300

    create estacao DTO

--- src/estacao/dto/create-estacao.dto.ts
+++ src/estacao/dto/create-estacao.dto.ts
@@ -1,1 +1,22 @@
-export class CreateEstacaoDto {}
+import { Prisma } from '@prisma/client';
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+import { Estacao } from '../entities/estacao.entity';
+
+export class CreateEstacaoDto implements Estacao {
+  id?: number;
+  @IsString()
+  status: string;
+  @IsString()
+  dataCadastro: string;
+  @IsString()
+  proximaAvaliacao: string;
+  @IsString()
+  deleted?: string;
+  @IsBoolean()
+  isObservationOpen: boolean;
+  @IsString()
+  lastUpdate: string;
+  @IsNumber()
+  reboleiraId?: number;
+  Raquetes?: Prisma.RaqueteUncheckedCreateNestedManyWithoutEstacaoInput;
+}
