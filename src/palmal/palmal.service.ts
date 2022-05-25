6ac336a4e12c285e2bbd4cf4189f68bc4ce7a375
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-05-25 17:44:48 -0300

    create function to create palmal

--- src/palmal/palmal.service.ts
+++ src/palmal/palmal.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePalmalDto } from './dto/create-palmal.dto';
 import { UpdatePalmalDto } from './dto/update-palmal.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Palmal } from './entities/palmal.entity';
 
 @Injectable()
 export class PalmalService {
-  create(createPalmalDto: CreatePalmalDto) {
-    return 'This action adds a new palmal';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreatePalmalDto): Promise<Palmal> {
+    const createdPalmal = await this.prisma.palmal.create({ data });
+
+    return createdPalmal;
   }
 
   findAll() {
