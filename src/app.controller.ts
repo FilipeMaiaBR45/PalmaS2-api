f55761b604b8a6cbc581bf353960d1f61ba42fc6
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2022-05-25 00:01:05 -0300

    add @IsPublic()

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
