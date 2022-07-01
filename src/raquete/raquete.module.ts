566c5cc33468e1fbc0d308dfd6e39b1620313813
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-07-01 17:16:23 -0300

    add provider PrismaService

--- src/raquete/raquete.module.ts
+++ src/raquete/raquete.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RaqueteService } from './raquete.service';
 import { RaqueteController } from './raquete.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [RaqueteController],
-  providers: [RaqueteService]
+  providers: [RaqueteService, PrismaService],
 })
 export class RaqueteModule {}
