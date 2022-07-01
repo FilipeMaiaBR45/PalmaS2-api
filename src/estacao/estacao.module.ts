1601d37c59245c34b6f1e0a75a5d451ab76aba61
FilipeMaiaBR45 <filopemaia2001@gmail.com>
2022-07-01 17:05:31 -0300

    add provider PrismaService

--- src/estacao/estacao.module.ts
+++ src/estacao/estacao.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EstacaoService } from './estacao.service';
 import { EstacaoController } from './estacao.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [EstacaoController],
-  providers: [EstacaoService]
+  providers: [EstacaoService, PrismaService],
 })
 export class EstacaoModule {}
